0e5f72df158801a906271ac81cd631df579c0ced
StasBeep <stas-stas.stanislav@mail.ru>
2023-06-02 01:47:53 +0300

    Добавление файлов для создания проекта на vue + подключение scss к проекту (добавление в основной репозиторий)

---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 :class="[$style.content]">Основная часть</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style module lang="scss" scoped>
+.content {
+    color: red;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <!--обёртка для всего содержимого, чтобы прижать footer к нижней части-->
+    <div class="wrapper-forcontent">
+        <!--Содержимое сайта без подвала (нужно для прижатия подвала к нижней части)-->
+        <div class="content-without-footer">
+
+            <!--Шапка сайта-->
+            <header>
+                <h1>Привет</h1>
+            </header>
+
+            <!--Основная часть-->
+            <main>
+                <!--Первая секция основной части-->
+                <section>
+                  <Hello/>
+                </section>
+                <!--Вторая секция-->
+                <section>
+
+                </section>
+            </main>
+        </div>
+
+        <!--Подвал сайта-->
+        <footer>
+          Версия: 0.1.0
+        </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Hello from '../components/Hello.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style src="../scss/style.scss" lang="scss" scoped>
+@import '../scss/style.scss';
+</style>
